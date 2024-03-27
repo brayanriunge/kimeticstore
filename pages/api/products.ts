e6428408674743db747972c7850ea455c0eb9202
RiugeBrian <riungebrayandev@gmail.com>
2024-03-27 21:47:39 +0300

    fetch data based on category

---
+++ pages/api/products.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/utils/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { category } = req.query;
+
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "method not allowed" });
+  }
+
+  try {
+    const products = prisma.product.findMany({
+      where: {
+        category: category as string,
+      },
+    });
+  } catch (error) {
+    console.log("Error fetching category products:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
