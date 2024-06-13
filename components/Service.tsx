f1d4db733af6b90b19e8a5cd56c8b92f0ef2ee34
RiugeBrian <riungebrayandev@gmail.com>
2024-06-13 22:35:04 +0300

    styling

--- components/Service.tsx
+++ components/Service.tsx
@@ -9,14 +9,45 @@ export default function Service() {
       id: 1,
       name: "Software Development",
     },
+    {
+      id: 2,
+      name: "Private Security",
+    },
+    {
+      id: 3,
+      name: "Private Jet Flights",
+    },
+    {
+      id: 4,
+      name: "Land Survey",
+    },
+    {
+      id: 5,
+      name: "Academic Consultation",
+    },
+    {
+      id: 6,
+      name: "International Investment Advisory",
+    },
+    {
+      id: 7,
+      name: "Partnership",
+    },
   ];
   return (
-    <div>
-      {services.map((service) => (
-        <div className="rounded-md text-montserrat px-8 p-2 shadow-md">
-          {service.name}
+    <div className=" ">
+      <div className="mx-auto w-5/6">
+        <div className=" p-5 ">
+          <h1 className="text-2xl font-semibold mb-2 ">Services:</h1>
+          <div className=" md:grid  md:grid-cols-4 gap-4 items-center">
+            {services.map((service) => (
+              <div className="rounded-md text-montserrat px-8 p-2 shadow-md hover:border-l-pink-200 hover:border-4">
+                {service.name}
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
