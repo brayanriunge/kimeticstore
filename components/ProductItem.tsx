54cdc1e5ce0d5df824fefbeba8953eece3a4e5fd
RiugeBrian <riungebrayandev@gmail.com>
2024-07-07 22:07:01 +0300

    redesigning card

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -19,7 +19,30 @@ export default function ProductItem({
       className="flex flex-col items-start justify-between gap-4 mx-auto mt-4 w-5/6  bg-white rounded-md shadow-sm hover:shadow-2xl "
       key={id}
     >
-      <div>
+      <div className="flex flex-col h-full max-w-sm rounded overflow-hidden shadow-lg ">
+        <Image
+          src={imgUrl}
+          alt="Card Image"
+          width={288} // Adjust the width based on your image
+          height={192} // Adjust the height based on your image
+          className="w-full h-auto"
+        />
+        <div className="flex-grow px-6 py-4">
+          <div className="font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base">
+            {description.slice(0, 20)}...
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <a
+            href="#"
+            className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+          >
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      {/* <div>
         <Link
           href={`/products/${id}`}
           className="md:relative block  cursor-pointer overflow-hidden rounded"
@@ -41,7 +64,7 @@ export default function ProductItem({
       <div className="flex items-center justify-between gap-2 mx-auto ">
         <h1 className="text-sm font-semibold">Description:</h1>
         <p className="text-md font-bold">{description.slice(0, 10)}...</p>
-      </div>
+      </div> */}
     </div>
   );
 }
