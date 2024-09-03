78a505d567d5d0f877aa63c62b2b397f3f522105
RiugeBrian <riungebrayandev@gmail.com>
2024-09-03 22:46:42 +0300

    remove console

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -46,7 +46,6 @@ const AdminDashboard = ({
     if (res.ok) {
       const userMessages: Message[] = await res.json();
       setMessages(userMessages);
-      console.log(userMessages);
     }
   };
 
