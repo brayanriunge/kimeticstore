86c68c79693e5de4e882b86935c0380f91bb9017
RiugeBrian <riungebrayandev@gmail.com>
2024-09-08 14:48:33 +0300

    changing roles activity

--- components/Auth/NavbarLoggedIn.tsx
+++ components/Auth/NavbarLoggedIn.tsx
@@ -37,6 +37,9 @@ export default function NavbarLoggedIn({ isLoggedIn, onSignOut }: NavbarProps) {
               <button className="rounded-md text-montserrat px-8 p-2 bg-orange-400">
                 <Link href={"/dashboard"}>Dashboard</Link>
               </button>
+              <button className="rounded-md text-montserrat px-8 p-2 bg-orange-400">
+                <Link href={"/admin"}>Chats</Link>
+              </button>
             </>
           )}
         </>
