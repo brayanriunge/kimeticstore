abea1aed4f69260c5352667618c4c4ab6fe2cefc
brayanriunge <145686424+brayanriunge@users.noreply.github.com>
2024-12-09 23:10:28 +0300

    correction

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <title>Home</title>
         <meta property="og:title" content="My page title" key="title" />
-        <link rel="icon" href="/LOGOjpeg" />
+        <link rel="icon" href="/LOGO.jpeg" />
       </Head>
       <Navbar />
       <TopDeal />
